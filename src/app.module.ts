86a82f01a4f4187d4b6cff5ae2a00f5af43e1b0f
Bruno Simplicio <bruno@notabene.id>
2024-04-12 15:44:15 -0400

    jwt strategy with Auth Guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
-import { AuthenticationController } from './controllers/authentication-controller'
+import { AuthenticationController } from './controllers/authentication.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -14,7 +15,11 @@ import { AuthModule } from './auth/auth.module'
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticationController],
+  controllers: [
+    CreateAccountController,
+    AuthenticationController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
