714e0cbd8f441b3b911d4f2c06a83d9ab808e533
Bruno Simplicio <bruno@notabene.id>
2024-04-16 20:45:22 -0400

    Answer questions controller

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -19,7 +19,7 @@ describe('Create Answer', () => {
 
   it('create an answer', async () => {
     const result = await sut.execute({
-      instructorId: 'any_id',
+      authorId: 'any_id',
       questionId: 'any_id',
       content: 'any_content',
       attachmentsIds: ['1', '2'],
---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,45 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+
+const AnswerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+const bodyValidationPipe = new ZodValidationPipe(AnswerQuestionBodySchema)
+
+type AnswerQuestionBodySchema = z.infer<typeof AnswerQuestionBodySchema>
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: AnswerQuestionBodySchema,
+    @Param('questionId') questionId: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
